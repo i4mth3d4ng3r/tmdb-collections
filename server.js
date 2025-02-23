784ba15c0d420ed82df2974a3a3101645efcf204
unknown <cullo64@gmail.com>
2025-02-23 10:59:31 +0100

    Auto-commit

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const { initializeBuffering } = require("./lib/bufferCatalogs");
 const PORT = process.env.PORT || 7000;
 const isDevelopment = process.argv.includes("--dev");
 
+console.log("PORT var type", typeof PORT);
+console.log("PORT", PORT);
+
 // create local server
 addon.listen(PORT, () => {
   console.log(`Addon active on port ${PORT}`);
@@ -10,7 +13,7 @@ addon.listen(PORT, () => {
   // Only run buffering in production environment
   if (isDevelopment) {
     console.log("Buffering disabled in development mode");
-  } else if (PORT === 5000) {
+  } else if (PORT == 5000) {
     console.log("Buffering disabled on port 5000"); // beamup deployment test runs on port 5000, which should be terminated (including buffering/schedule) due to container removal after the test is completed. But for some reason buffering continues and is scheduled. Which causes duplicated bufferings.
   } else {
     console.log("Starting catalog buffering system...");
